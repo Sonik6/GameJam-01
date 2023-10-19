f81e5adb3f755082dcdd202f981aad8dd4cff25d
minislel <marcin4kt@gmail.com>
2023-10-20 01:01:30 +0200

    dfs

--- GameJam-01/Assets/Scripts/Gate.cs
+++ GameJam-01/Assets/Scripts/Gate.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class Gate : MonoBehaviour
 {
+<<<<<<< Updated upstream
    private AudioSource audioData;
 
+=======
+    private AudioSource audioData;
+    private bool bossResp=false;
+>>>>>>> Stashed changes
     public int coinRequirement = 1;
     // Start is called before the first frame update
     void Start()
     {
         audioData = GameObject.Find("Main Camera").GetComponent<AudioSource>();
+<<<<<<< Updated upstream
         Debug.Log(audioData.clip);
+=======
+>>>>>>> Stashed changes
 
     }
 
@@ -20,7 +28,20 @@ public class Gate : MonoBehaviour
     {
         if (coinRequirement <= gameManager.points)
         {
+<<<<<<< Updated upstream
             audioData.Play(0);
+=======
+            audioData.clip = Resources.Load<AudioClip>("muzyka/gate");
+            audioData.Play();
+
+            if (!bossResp)
+            {
+                Vector3 spawnPosition = new Vector3(-7.87f, 2.92f, 0f);
+                Instantiate(spawnableObject, spawnPosition, Quaternion.identity);
+                bossResp = true;
+            }
+            GetComponent<AudioSource>().Play();
+>>>>>>> Stashed changes
             Destroy(gameObject);
 
         }
