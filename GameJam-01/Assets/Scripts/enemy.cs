3b54b3c98c40bb1bb0724a216f07589a0cae8884
minislel <marcin4kt@gmail.com>
2023-10-19 16:08:51 +0200

    health

--- GameJam-01/Assets/Scripts/enemy.cs
+++ GameJam-01/Assets/Scripts/enemy.cs
@@ -13,13 +13,16 @@ public class enemyBehaviour : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.AddForce(new Vector2(movingSpeed, 0));
+        //rb.AddForce(new Vector2(movingSpeed, 0));
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
+        { 
             Destroy(gameObject);
+
+        }
 
         if (collision.gameObject.tag == "wall")
         {
@@ -31,7 +34,13 @@ public class enemyBehaviour : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
+        {
+
+            playerHealth.health--;
+            gameManager.Death();
+            Debug.Log("player health" + playerHealth.health);
             Destroy(collision.gameObject);
+        }
     }
 
     // Update is called once per frame
--- GameJam-01/Assets/Scripts/gameManager.cs
+++ GameJam-01/Assets/Scripts/gameManager.cs
@@ -1,34 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class gameManager : MonoBehaviour
 {
     public GameObject playerObject;
-    public int playerHealth;
+    public GameObject enemyObject;
+    UnityEvent playerDamage;
+    
+    private int healthCheck;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerHealth = 3;
+        
+        healthCheck = playerHealth.health;
+    }
+    public static void Death()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
 
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    { }
 
 
     // Update is called once per frame
     void Update()
     {
-        playerObject = GameObject.Find("PlayerCharacter");
-        Debug.Log(playerObject);
-        double playerPosY = playerObject.transform.position.y;
-        Debug.Log(playerPosY);
-        if (playerPosY < -6)
-        {
-            SceneManager.LoadScene(sceneName: "MainMenu");
+        Debug.Log("health: " + playerHealth.health);
+        Debug.Log("healthcheck:" + healthCheck);
 
-
-
+            if (playerHealth.health > 0)
+            {
+                healthCheck = playerHealth.health;
+                playerObject = GameObject.Find("PlayerCharacter");
+                double playerPosY = playerObject.transform.position.y;
+                if (playerPosY < -6)
+                {
+                    playerHealth.health = 0;
+                }
+            }
+            if (playerHealth.health <= 0)
+            {
+                healthCheck = playerHealth.health;
+                SceneManager.LoadScene(sceneName: "MainMenu");
+            }
         }
+
     }
-}
+    
+
+
