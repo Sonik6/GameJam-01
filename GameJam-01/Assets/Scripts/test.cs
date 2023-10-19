426cb3230a9479f2958e9802586804c9f7a49c8b
minislel <marcin4kt@gmail.com>
2023-10-19 21:16:06 +0200

    sad

--- GameJam-01/Assets/Scripts/test.cs
+++ GameJam-01/Assets/Scripts/test.cs
@@ -1,21 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
-public class coinCounter : MonoBehaviour
+public class test : MonoBehaviour
 {
-    public static TMP_Text displayCoins;
-
     // Start is called before the first frame update
     void Start()
     {
-        displayCoins = GetComponent<TMP_Text>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-         
+        
     }
 }
