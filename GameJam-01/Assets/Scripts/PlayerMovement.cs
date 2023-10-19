ae0eaf3bf51f2098f374872c5bbd4ce622ba75e6
minislel <marcin4kt@gmail.com>
2023-10-19 11:57:47 +0200

    dolozone chodzenie

--- GameJam-01/Assets/Scripts/PlayerMovement.cs
+++ GameJam-01/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float jumpForce;
+
+    
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
@@ -15,10 +17,14 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float moveSpeed = 10;
+        transform.Translate(new Vector2(horizontalInput, 0) * moveSpeed * Time.deltaTime);
         if (Input.GetButtonDown("Jump"))
         {
             rb.AddForce(new Vector2(rb.velocity.x, jumpForce));
         }
+        
         {
             
         }
